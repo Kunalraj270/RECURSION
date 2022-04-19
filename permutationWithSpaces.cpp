e43970efec5cc035d1a269976553c28e67593b06
Kunalraj270 <kunalraj2999@gmail.com>
2022-04-19 13:28:16 +0530

    pws

---
+++ permutationWithSpaces.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(string input , string output){
+    if(input.size() == 0){
+        cout<<output<<endl;
+        return;
+    }
+
+    string output1 = output;
+    string output2 = output;
+
+    output1.push_back('_');
+    output1.push_back(input[0]);
+
+    output2.push_back(input[0]);
+    input.erase(input.begin() + 0);
+
+    solve(input , output1);
+    solve(input , output2);
+
+    return;
+
+
+}
+
+
+int main(){
+
+    string input;
+    cin>>input;
+
+    string output = "";
+
+    output.push_back(input[0]);
+    input.erase(input.begin() + 0);
+
+    solve(input , output);    
+}
